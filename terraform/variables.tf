22f3871ae4c730a8ecf483eb8e0d1a858b8d9809
Eglodji1618 <124696200+Eglodji1618@users.noreply.github.com>
2023-10-27 08:40:20 -0400

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "East US"
+}
+
+variable "resource_group_name" {
+  description = "Resource Group Name for resources been created"
+  type        = string
+  default     = "restaurantfetch"
+}
+
+variable "administrator_login" {
+  description = "User name for Mysql Server Database"
+  type        = string
+  default     = "mysqladmin"
+}
+
+variable "administrator_login_password" {
+  description = "Login Password for Mysql Database"
+  type        = string
+  default     = "assignment#123"
+}
+
+variable "repo_url" {
+  description = "URL for the Git Repo to be deployed."
+  type        = string
+  default     = ""
+}
